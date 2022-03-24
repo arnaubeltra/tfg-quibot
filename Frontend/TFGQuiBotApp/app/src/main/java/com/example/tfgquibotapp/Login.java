2f4319569054e0825ded1d4c1ef1aa78ed09c8c7
arnaubeltra <80287370+arnaubeltra@users.noreply.github.com>
2022-03-24 18:20:24 +0100

    Frontend: App Login and Register

---
+++ Frontend/TFGQuiBotApp/app/src/main/java/com/example/tfgquibotapp/Login.java
@@ -0,0 +1,78 @@
+package com.example.tfgquibotapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    private EditText emailUser, passwordUser;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private ProgressDialog loginDialog;
+    private String emailPattern = "[a-zA-Z0-9._-]+@+[a-z]+\\.+[a-z]+";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailUser = findViewById(R.id.inputTxtEmail);
+        passwordUser = findViewById(R.id.inputTxtPassword);
+
+        findViewById(R.id.btnLogin).setOnClickListener(view -> login());
+        findViewById(R.id.textViewCreateNewAccount).setOnClickListener(view -> startActivity(new Intent(Login.this, Register.class)));
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        loginDialog = new ProgressDialog(this);
+    }
+
+    private void login() {
+        String email = emailUser.getText().toString();
+        String password = passwordUser.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            emailUser.setError("Enter correct email");
+        } else if ((password.isEmpty()) || (password.length() < 6)) {
+            passwordUser.setError("Password length must be higher than 6");
+        } else {
+            loginDialog.setMessage("Espera mentre s'inicia sessió");
+            loginDialog.setTitle(R.string.txtLoggingIn);
+            loginDialog.setCanceledOnTouchOutside(false);
+            loginDialog.show();
+
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    loginDialog.dismiss();
+                    goToHomeActivity();
+                } else {
+                    loginDialog.dismiss();
+                    Toast.makeText(Login.this, R.string.txtErrorLoggingIn, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void goToHomeActivity() {
+        Intent intent = new Intent(Login.this, Home.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
---
+++ Frontend/TFGQuiBotApp/app/src/main/java/com/example/tfgquibotapp/Register.java
@@ -0,0 +1,85 @@
+package com.example.tfgquibotapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity {
+
+    private EditText emailRegister, passwordRegister, confirmPasswordRegister;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private ProgressDialog registerDialog;
+    private String emailPattern = "[a-zA-Z0-9._-]+@+[a-z]+\\.+[a-z]+";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailRegister = findViewById(R.id.inputTxtEmailRegister);
+        passwordRegister = findViewById(R.id.inputTxtPasswordRegister);
+        confirmPasswordRegister = findViewById(R.id.inputTxtConfirmPasswordRegister);
+
+        findViewById(R.id.btnCreateAccountRegister).setOnClickListener(view -> registerNewUser());
+        findViewById(R.id.textViewAlreadyHaveAnAccount).setOnClickListener(view -> finish());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        registerDialog = new ProgressDialog(this);
+    }
+
+    private void registerNewUser() {
+        String email = emailRegister.getText().toString();
+        String password = passwordRegister.getText().toString();
+        String confirmPassword = confirmPasswordRegister.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            emailRegister.setError("Enter correct email");
+        } else if ((password.isEmpty()) || (password.length() < 6)) {
+            passwordRegister.setError("Password length must be higher than 6");
+        } else if (!password.equals(confirmPassword)) {
+            confirmPasswordRegister.setError("Passwords do not match");
+        } else {
+            registerDialog.setMessage("Espera mentre es realitza el procés de registre");
+            registerDialog.setTitle(R.string.registering);
+            registerDialog.setCanceledOnTouchOutside(false);
+            registerDialog.show();
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        registerDialog.dismiss();
+                        goToHomeActivity();
+                        Toast.makeText(Register.this, R.string.newUserRegistered, Toast.LENGTH_SHORT).show();
+                    } else {
+                        registerDialog.dismiss();
+                        Toast.makeText(Register.this, R.string.txtErrorRegistering, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void goToHomeActivity() {
+        Intent intent = new Intent(Register.this, Home.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
